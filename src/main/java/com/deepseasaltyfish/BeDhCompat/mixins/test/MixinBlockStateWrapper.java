bfd855a271c2e086e43b60935706a0927041b6a1
Deepseasaltyfish <h20040215@163.com>
2026-04-14 17:01:51 +0800

    add MixinBlockStateWrapper to fix DH2.4.0+ to fix Lod problem temperately

---
+++ src/main/java/com/deepseasaltyfish/BeDhCompat/mixins/test/MixinBlockStateWrapper.java
@@ -0,0 +1,35 @@
+package com.deepseasaltyfish.BeDhCompat.mixins.test;
+
+import com.seibel.distanthorizons.core.logging.DhLogger;
+import com.seibel.distanthorizons.core.util.LodUtil;
+import loaderCommon.forge.com.seibel.distanthorizons.common.wrappers.block.BlockStateWrapper;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.annotation.Nullable;
+
+@Mixin(BlockStateWrapper.class)
+public abstract class MixinBlockStateWrapper {
+
+    @Shadow(remap = false)
+    @Nullable
+    public BlockState blockState;
+
+    @Shadow @Final private static DhLogger LOGGER;
+
+    @Inject(method = "calculateOpacity", at = @At("HEAD"), cancellable = true, remap = false)
+    private void onCalculateOpacity(CallbackInfoReturnable<Integer> cir) {
+        if (this.blockState != null) {
+            String blockName = this.blockState.getBlock().toString();
+            if (blockName.equals("Block{littletiles:tiles}")) {
+//                LOGGER.warn("changed Opacity for LT block");
+                cir.setReturnValue(LodUtil.BLOCK_FULLY_OPAQUE);
+            }
+        }
+    }
+}
