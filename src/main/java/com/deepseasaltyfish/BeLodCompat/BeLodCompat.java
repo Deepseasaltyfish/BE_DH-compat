d0cabbfa387075ddad00745bc761ffccd8962159
Deepseasaltyfish <h20040215@163.com>
2026-04-26 01:47:58 +0800

    Revert "revert change about color api to mixin and wait"
    This reverts commit 3c50b2897bf2d350b05abbdfa6c3564b895c16da.

--- src/main/java/com/deepseasaltyfish/BeLodCompat/BeLodCompat.java
+++ src/main/java/com/deepseasaltyfish/BeLodCompat/BeLodCompat.java
@@ -39,9 +39,8 @@ public class BeLodCompat {
         BlockStateOpacityReplacer blockStateOpacityReplacer = new BlockStateOpacityReplacer();
         DhApi.events.bind(DhApiBlockStateWrapperCreatedEvent.class, blockStateOpacityReplacer);
 
-        //TODO: revert this until the api could be used...
-//        BlockColorReplacer blockColorReplacer = new BlockColorReplacer();
-//        DhApi.events.bind(DhApiBlockColorOverrideEvent.class, blockColorReplacer);
+        BlockColorReplacer blockColorReplacer = new BlockColorReplacer();
+        DhApi.events.bind(DhApiBlockColorOverrideEvent.class, blockColorReplacer);
     }
 
     private void onClientSetup(final FMLClientSetupEvent event) {
